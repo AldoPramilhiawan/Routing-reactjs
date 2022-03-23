82e36d2a18bc9d26e7c9678227cbd6053f558968
Aldo Pramilhiawan <aldopramilhiawan0@gmail.com>
2022-03-23 21:00:15 +0700

    baru

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import About from "./Component/About";
+import Contact from "./Component/Contact";
+import Home from "./Component/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
+import './index.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand">Navbar</a>
+          <ul class="nav justify-content-end">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/home">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contact">
+                Contact
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">
+                About
+              </Link>
+            </li>
+          </ul>
+
+        </div>
+      </nav>
+
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Component/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+
+function Home() {
+  return (
+    <div className="container text-center my-3">
+     <div className="p-5 mb-4 bg-light rounded-3 jumbroton">
+      <div className="container-fluith py-5 text-center">
+        <p className="fs-5 fst-italic mt-5">Front End Developer</p>
+        <h1 className="display-4">Aldo Pramilhiawan Kencoko</h1>
+        <button className="btn btn-warning border border-dark btn-sm mt-5 shadow-sm text-dark px-3 py-2">Likedin Profile</button>
+      </div>
+    </div>
+
+    </div>
+  );
+}
+
+export default Home;
